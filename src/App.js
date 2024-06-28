1549ad5b95fb5af94d530f394bbbbac7543c67fb
Matias Santos <santos.matias.a.97@gmail.com>
2024-06-28 17:28:48 -0300

    2 | blog-personal | Navigation bar routing done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import LogIn from "./Pages/Login";
+import SignIn from "./Pages/SignIn";
+import CreateItem from "./Pages/CreateItem";
+import Cart from "./Pages/Cart";
+import Checkout from "./Pages/Checkout";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/create-item" element={<CreateItem />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
